af155c582d500a1d7947d02ca418e278b49620d6
nrdev <nelson_rojas_janda@hotmail.com>
2022-03-31 11:11:10 +0200

    Add function to nav button

---
+++ app.js
@@ -0,0 +1,10 @@
+// === === === Variables === === ===
+const navBtn = document.getElementById('navBtn');
+
+// === === === Functions === === ===
+const openNav = () => {
+    console.log ('work');
+};
+
+// === === === Events === === ===
+navBtn.addEventListener('click', openNav);
